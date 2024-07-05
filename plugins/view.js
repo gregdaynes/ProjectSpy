d94bfc687e199cce82c33a0071cbb439f4120bed
Gregory Daynes <email@gregdaynes.com>
2024-07-05 14:58:59 -0700

    testing most approaches done

--- plugins/view.js
+++ plugins/view.js
@@ -5,12 +5,12 @@ import nunjucks from 'nunjucks'
 
 export default fp(
   async function pluginView (fastify, _opts) {
-    fastify.register(fastifyView, {
-      engine: { nunjucks },
-      templates: [join(import.meta.dirname, '..', 'views')],
-      viewExt: 'njk',
-      propertyName: 'render',
-    })
+  //   fastify.register(fastifyView, {
+  //     engine: { nunjucks },
+  //     templates: [join(import.meta.dirname, '..', 'views')],
+  //     viewExt: 'njk',
+  //     propertyName: 'render',
+  //   })
   },
   {
     dependencies: ['application-config', 'list-files'],
--- routes/webc.js
+++ routes/webc.js
@@ -1,5 +1,22 @@
+import { WebC } from '@11ty/webc'
+import { join } from 'node:path'
+
 export default async function (fastify) {
   fastify.get('/webc', async (request, reply) => {
-    return { test: 'test' }
+    const page = new WebC()
+    page.defineComponents(join(import.meta.dirname, 'webc', '**.webc'))
+    page.setInputPath(join(import.meta.dirname, 'webc', 'layout.webc'))
+
+    const data = {
+      user: 'testy mctesterson',
+      num: 3,
+    }
+
+    const { html } = await page.compile({ data })
+
+    reply.headers({
+      'Content-Type': 'text/html',
+    })
+    return reply.send(html)
   })
 }
