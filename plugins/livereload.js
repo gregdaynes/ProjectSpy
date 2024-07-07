3100717c6d60738bcdbde8a8f2230d43e29f8a2b
Gregory Daynes <email@gregdaynes.com>
2024-07-06 23:27:15 -0700

    initial setup of endpoint, livereload.js and sockets
    socket responding with protocol data
    
    working dynamic css file based on route and buffer
    
    rebuild webc on file change
    
    working - needs cleanup, but live reload css works
    
    refactoring - remove need for event emitter and reduce coupling
    
    support multiple pages and more intelligent live reload
    
    refactor - clean up and extract / abstract
    
    remove old webc test file

---
+++ plugins/livereload.js
@@ -0,0 +1,68 @@
+import fp from 'fastify-plugin'
+import { join } from 'node:path'
+import { createReadStream } from 'node:fs'
+
+export default fp(async (fastify, opts) => {
+  opts = Object.assign({}, {
+    liveCSS: true,
+  }, opts)
+
+  await fastify.register(import('@fastify/websocket'))
+
+  fastify.decorate('generateLiveReloadScript', function () {
+    return `<script>
+      document.write('<script src="http://'
+        + (location.host || 'localhost').split(':')[0]
+        + ':3000/livereload.js"></'
+        + 'script>')
+      </script>`
+  })
+
+  fastify.decorate('sendLiveReloadCommand', function (command, path) {
+    fastify.websocketServer.clients.forEach((client) => {
+      client.send(JSON.stringify({
+        command,
+        path,
+        liveCSS: opts.liveCSS,
+      }))
+    })
+  })
+
+  fastify.get('/livereload.js', (req, reply) => {
+    const filePath = join(import.meta.dirname, '..', 'vendor', 'livereload.4_0_2.min.js')
+
+    const stream = createReadStream(filePath, 'utf8')
+    reply.header('Content-Type', 'text/javascript; charset=utf-8')
+    reply.send(stream)
+  })
+
+  fastify.get('/livereload', { websocket: true }, (socket, request) => {
+    socket.on('message', message => {
+      message = JSON.parse(message.toString())
+
+      if (message.command === 'hello') {
+        request.log.debug('Client requested handshake...')
+
+        const data = JSON.stringify({
+          command: 'hello',
+          protocols: [
+            'http://livereload.com/protocols/official-7',
+            'http://livereload.com/protocols/official-8',
+            'http://livereload.com/protocols/official-9',
+            'http://livereload.com/protocols/2.x-origin-version-negotiation',
+            'http://livereload.com/protocols/2.x-remote-control'],
+          serverName: 'fastify-livereload',
+        })
+
+        return socket.send(data)
+      }
+
+      if (request.command === 'info') {
+        request.log.debug({ message }, 'Client requested info...')
+      }
+    })
+  })
+}, {
+  name: 'livereload',
+  dependencies: ['application-config'],
+})
