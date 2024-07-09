d6012f68e22bf0677465d18eed84c021d0c9bd92
Gregory Daynes <email@gregdaynes.com>
2024-07-09 08:20:51 -0700

    wip - edit interface

---
+++ .projectspy/Backlog/envars.md
@@ -0,0 +1,7 @@
+It's time for envars / runtime flags!
+
+With things like live-reload in place for development, environment variables
+and runtime flags are needed.
+
+By default they should not be needed to start the application for the optimal
+configuration for an end user.
--- routes/update.js
+++ routes/update.js
@@ -10,6 +10,16 @@ export default async function (fastify) {
     // const task = request.taskList().get(filePath)
     const fileContents = await readFile(filePath, { encoding: 'utf-8' })
 
+    // TODO this is gonna be rough. We should clean and hash both to determine if they are the same
+    const newFileContents = request.body.content
+    if (fileContents !== newFileContents) {
+		  // TODO we should combine the writes, so their is only a single disk write at the end
+      await writeFile(filePath, newFileContents)
+      // TODO we also need to force update the task in our taskList
+      const newTask = await TaskFactory(filePath, request.server.config.dirPath)
+      request.updatePath(filePath, filePath, newTask)
+    }
+
     // move the file if the lane has changed
     if (lane !== request.body.lane) {
       lane = request.body.lane
