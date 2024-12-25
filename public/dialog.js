a9cdf1d7c7055aef0cf759d835b8e9e5272dabca
Gregory Daynes <gregdaynes@gmail.com>
2024-12-24 17:45:54 -0800

    chore: closing dialog should be deliberate
    Prevent dialog from closing when a click starts elsewhere but ends on
    the backdrop.

--- public/dialog.js
+++ public/dialog.js
@@ -6,8 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     dialog.showModal()
 
-    dialog.addEventListener('click', ({ target }) => {
-      if (target === dialog) {
+    /**
+     *  Close dialog clicking on backdrop
+     *  the backdrop is considered to be part of the dialog
+     *  we check for a click on the dialog and if true calls close on the dialog
+     *  This introduces a problem where any click anywhere on the actual dialog
+     *  closes the dialog. To resolve that, we cover the entire dialog in elements
+     *  and remove padding, so you're never clicking on the dialog except when
+     *  clicking the backdrop.
+     *
+     *  Additionally, if a click starts on the dialog, but ends up on the backdrop
+     *  this should not close the dialog. Clicking the backdrop should be a
+     *  deliberate action.
+     */
+    let downTarget
+    dialog.addEventListener('mousedown', ({ target }) => {
+      downTarget = target
+    })
+
+    dialog.addEventListener('mouseup', ({ target }) => {
+      if (target === downTarget && target === dialog) {
         dialog.close()
       }
     })
