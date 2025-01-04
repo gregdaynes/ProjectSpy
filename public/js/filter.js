fc544eb3e2f7b6203582b46d5753e193c19969aa
Gregory Daynes <gregdaynes@gmail.com>
2025-01-03 23:18:18 -0800

    chore: case insensitive filter with highlight

--- public/js/filter.js
+++ public/js/filter.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const results = filterData.filter(([entry]) => {
-      return entry.includes(e.target.value)
+      return entry.includes(e.target.value.toLowerCase())
     }).map(([, id]) => id)
 
     for (const task of tasks) {
@@ -50,6 +50,21 @@ document.addEventListener('DOMContentLoaded', () => {
         task.classList.add('hidden')
       } else {
         task.classList.remove('hidden')
+
+        const taskHeader = task.querySelector('task-header')
+        const taskBody = task.querySelector('task-body')
+
+        if (!task.hasAttribute('data-header-original')) {
+          task.setAttribute('data-header-original', taskHeader.innerHTML)
+        }
+
+        if (!task.hasAttribute('data-body-original')) {
+          task.setAttribute('data-body-original', taskBody.innerHTML)
+        }
+
+        const regex = new RegExp(e.target.value, 'ig')
+        taskHeader.innerHTML = task.getAttribute('data-header-original').replace(regex, '<mark>$&</mark>')
+        taskBody.innerHTML = task.getAttribute('data-body-original').replace(regex, '<mark>$&</mark>')
       }
     }
   })
