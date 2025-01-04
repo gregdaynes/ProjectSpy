16f9989086e807c1954a12578a1b5a6b11b678ef
Gregory Daynes <gregdaynes@gmail.com>
2025-01-03 22:51:19 -0800

    refactor: cleanup server.js script

--- bin/server.js
+++ bin/server.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 
-// Read the .env file.
-import { join } from 'path'
-import open from 'open'
 import { parseArgs } from 'node:util'
-
-// Require the framework
+import open from 'open'
 import Fastify from 'fastify'
-
-// Require library to exit fastify process, gracefully (if possible)
 import closeWithGrace from 'close-with-grace'
+import app from '../index.js'
 
-const { positionals, } = parseArgs({
+const { positionals } = parseArgs({
   options: {
     init: {
       type: 'boolean',
@@ -24,41 +19,29 @@ const { positionals, } = parseArgs({
   allowPositionals: true
 })
 
-const cmd = positionals[0]
-const config = {
+const fastify = Fastify({
   logger: true,
-  pluginTimeout: cmd ? 60000 : 1000,
-}
-
-// Instantiate Fastify with some config
-const app = Fastify(config)
-
-// Register your application as a normal plugin.
-app.register(await import(join(import.meta.dirname, '..', 'index.js')), {
-  cmd: positionals[0]
+  pluginTimeout: positionals[0] ? 60000 : 1000,
 })
 
-// delay is the number of milliseconds for the graceful close to finish
-const closeListeners = closeWithGrace({ delay: process.env.FASTIFY_CLOSE_GRACE_DELAY || 500 }, async function ({ signal, err, manual }) {
-  if (err) {
-    app.log.error(err)
-  }
-  await app.close()
+fastify.register(app)
+
+const closeListeners = closeWithGrace({ delay: 500 }, async ({ err }) => {
+  if (err) fastify.log.error(err)
+  await fastify.close()
 })
 
-app.addHook('onClose', async (instance, done) => {
+fastify.addHook('onClose', async () => {
   closeListeners.uninstall()
-  done()
 })
 
-// Start listening.
 try {
-  await app.listen({ port: process.env.PORT || 8080 })
+  await fastify.listen({ port: process.env.PORT || 8080 })
 
-  const { address, port } = app.addresses().find((address) => address.family === 'IPv4')
+  const { address, port } = fastify.addresses().find((address) => address.family === 'IPv4')
 
   await open(`http://${address}:${port}`)
 } catch (err) {
-  app.log.error(err)
+  fastify.log.error(err)
   process.exit(1)
 }
